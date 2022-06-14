fb6e53abb830162a78c95c6e904460dfcb4b7d91
André <anz7re@autokit.tech>
2022-06-13 22:06:38 -0400

    Update check_url.bat

--- shell/check_url.bat
+++ shell/check_url.bat
@@ -4,14 +4,15 @@ setlocal EnableDelayedExpansion
 if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
 
 set X=0
-set dr=C:
-set env=%HOMEPATH%
+set env=%USERPROFILE%
 set loc=\.z7\autokit\etweetxl\mtsett\webcheck.txt
+
+echo/> %env%%loc%
 
 for /f "tokens=10 delims=," %%A in ('tasklist /fo csv /v /fi "imagename eq firefox.exe"' ) do set /a X+=1 && set URL=%%A && if !X!==1 goto EscLoop 
 exit
 
 :EscLoop
-echo %URL% > %dr%%env%%loc%
+echo %URL% > %env%%loc%
 
 exit
